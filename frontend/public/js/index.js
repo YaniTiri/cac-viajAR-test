26e9ba54f434662a589a21d4ecaf5cce743e6abd
Yanina Tiribelli <yaninatiribelli@gmail.com>
2024-06-27 18:45:08 -0300

    correccion url

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const fetchPaquetes = async ()=>{
 
     try {
-      const respuesta = await axios(`http://localhost:3001/paquetes`);
+      const respuesta = await axios(urlApi+`/paquetes`);
       //console.log(respuesta.data);
       const paquetes = respuesta.data;
       //Borrar todo antes de cargar?
