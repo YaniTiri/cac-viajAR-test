734fbc7edd5ca67e30bfbc4a89bb6a5d266faf90
Yanina Tiribelli <yaninatiribelli@gmail.com>
2024-06-27 18:29:59 -0300

    guardando variables

--- backend/data/bd.js
+++ backend/data/bd.js
@@ -9,11 +9,13 @@ require('dotenv').config();
      port:3306
  })*/
 
- const db = new Sequelize ("viaja2024_viajar2024",process.env.DB_USER,process.env.DB_PASSWORD,{
+ const db = new Sequelize (process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect: "mysql",
     port:3306
 }) 
+
+
 
 module.exports = db
 
