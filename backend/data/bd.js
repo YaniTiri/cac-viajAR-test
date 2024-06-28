a3a40f8af1fc748164646ecac123fdf72a230d54
Yanina Tiribelli <yaninatiribelli@gmail.com>
2024-06-28 15:29:10 -0300

    bd host

--- backend/data/bd.js
+++ backend/data/bd.js
@@ -10,7 +10,7 @@ require('dotenv').config();
  })*/
 
  const db = new Sequelize (process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
+    host: "mysql-yanina.alwaysdata.net",
     dialect: "mysql",
     port:3306
 }) 
